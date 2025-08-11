ca3508714d0df1eca73cd8a308321544ca639548
Greg W <1514406+fatbotgw@users.noreply.github.com>
2025-08-11 16:28:55 -0400

    Added constants file with dimensions for various items

---
+++ src/constants.py
@@ -0,0 +1,24 @@
+# Pallet dimenions:
+#   all dimensions are nominal and leave off tolerances
+# Pallet = (Long, Short, Height) <<-- millimeters
+D430 = (1061, 760, 172.5)
+D330B = (1200, 800, 175)
+D525 = (1075, 769, 175)
+D540 = (1068, 760, 173.3)
+D541 = (1468, 1063, 173.5)
+D644 = (1050, 760, 80)
+
+
+# Shipping Containers
+#   dimensions are internal and approximate
+# CONT = (Long, Short, Height) <<-- meters
+CONT_10 = (2.84, 2.28, 2.26)
+CONT_20 = (5.44, 2.28, 2.26)
+CONT_40 = (12.04, 2.28, 2.26)
+CONT_40HC = (12.04, 2.89, 2.26)
+
+# Semi Trailers
+#   dimensions are internal and approximate
+# Trailer = (Long, Short, Height) <<-- feet, inch, inch
+TRAILER_53 = (52.5, 99, 110)
+TRAILER_48 = (47.5, 99, 110)
