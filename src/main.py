3e98c24018f3b040fef1c19d51804e6b1a09c2cf
Greg W <1514406+fatbotgw@users.noreply.github.com>
2025-08-13 22:45:36 -0400

    Added total counts for a full container

--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ def main():
 
     #TODO the container size should be chosen by the user
     container_size = CONT_40
+    container_label = "40' Container"
     
     #TODO these values should use appropriate conversions based on container units
     length = container_size[0] * ureg.meter
@@ -23,23 +24,26 @@ def main():
 
 
     #TODO the pallet model should be chosen by the user
-    if D430[0] > 1060:
+    pallet_model = D430
+    if pallet_model[0] > 1060:
       print("Long side is larger")
 
-    footprint_L = D430[0] * ureg.mm
-    print(footprint_L)
+    footprint_L = pallet_model[0] * ureg.mm
     fit_count_L = length // footprint_L.to("meter")
     print(f"how many will fit (length): {fit_count_L.magnitude}")
 
-    footprint_S = D430[1] * ureg.mm
-    print(footprint_S)
+    footprint_S = pallet_model[1] * ureg.mm
     fit_count_S = width // footprint_S.to("meter")
     print(f"how many will fit (width): {fit_count_S.magnitude}")
 
-    footprint_H = D430[2] * ureg.mm
-    print(footprint_H)
+    footprint_H = pallet_model[2] * ureg.mm
     fit_count_H = height // footprint_H.to("meter")
     print(f"how tall can the stack be: {fit_count_H.magnitude}")
+
+    fit_count_stacks = fit_count_L.magnitude * fit_count_S.magnitude
+    print(f"You can fit {fit_count_stacks} stacks in a {container_label}.")
+    fit_count_total = fit_count_stacks * fit_count_H.magnitude
+    print(f"That means you can fit {fit_count_total} pallets in a {container_label}.")
 
 if __name__ == "__main__":
    main()
